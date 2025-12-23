ebfae8a07d71870b690663c261ed86f157eeef22
Kenan Causevic <fashslash60@gmail.com>
2025-12-23 19:20:34 +0100

    Module 7 i mini ch

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Main" component={MenuScreen}></Stack.Screen>
+        <Stack.Screen name="Main" component={MainScreen}></Stack.Screen>
          <Stack.Screen name="List" component={ListScreen}></Stack.Screen>
         <Stack.Screen name="Home" component={ButtonScreen}></Stack.Screen>
       </Stack.Navigator>
